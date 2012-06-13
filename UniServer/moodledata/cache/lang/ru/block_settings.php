e648541a502b11f6498173b199ece9b523f78ba9
Sergey Monin <moninsergei@gmail.com>
2012-06-13 20:39:55 +0400

    Deleted some files, updated start page

--- UniServer/moodledata/cache/lang/ru/block_settings.php
+++
@@ -1,4 +0,0 @@
-<?php $this->cache['ru']['block_settings'] = array (
-  'enabledock' => 'Разрешить перемещать этот блок в док',
-  'pluginname' => 'Настройки',
-);
--- UniServer/moodledata/cache/lang/ru/block_site_main_menu.php
+++
@@ -1,3 +0,0 @@
-<?php $this->cache['ru']['block_site_main_menu'] = array (
-  'pluginname' => 'Основное меню',
-);
